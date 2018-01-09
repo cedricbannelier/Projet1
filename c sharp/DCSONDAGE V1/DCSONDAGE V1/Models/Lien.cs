82cc7be09f68e8a815d51b68d6402fd4e2288904
Dom Renju <zyroprimefr@hotmail.com>
2018-01-09 17:02:23 +0100

    ajout c sharp

---
+++ c sharp/DCSONDAGE V1/DCSONDAGE V1/Models/Lien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DCSONDAGE_V1.Models
+{
+    public class Lien
+    {
+        public String AdresseLien1 { get; set; }
+        public String AdresseLien2 { get; set; }
+        public String AdresseLien3 { get; set; }
+        public Lien(String Adresse1, String Adresse2, String Adresse3)
+        {
+            AdresseLien1 = Adresse1;
+            AdresseLien2 = Adresse2;
+            AdresseLien3 = Adresse3;
+        }
+    }
+}
---
+++ c sharp/DCSONDAGE V1/DCSONDAGE V1/Models/Sondage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DCSONDAGE_V1.Models
+{
+    public class Sondage
+    {
+        public String QuestionSondage { get; set; }
+        public Boolean ChoixUniqueSondage { get; set; }
+
+        public Sondage(string question, Boolean choix)
+        {
+            QuestionSondage = question;
+            ChoixUniqueSondage = choix;
+        }
+    }
+}
